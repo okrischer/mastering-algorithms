34df2da076b34bf645b59dae6e8562a91ff1ded3
Oliver Krischer <oliver.krischer@gmail.com>
2023-11-21 20:49:09 +0100

    completed stacks

--- test/hanoi.c
+++ test/hanoi.c
@@ -3,8 +3,6 @@
 #include "stack.h"
 #include "test.h"
 
-#define DISCS 8
-
 void initialize(Stack* start, Stack* temp, Stack* goal) {
     stack_init(start, NULL);
     stack_init(temp, NULL);
@@ -13,17 +11,7 @@ void initialize(Stack* start, Stack* temp, Stack* goal) {
     static int disc1 = 1;
     static int disc2 = 2;
     static int disc3 = 3;
-    static int disc4 = 4;
-    static int disc5 = 5;
-    static int disc6 = 6;
-    static int disc7 = 7;
-    static int disc8 = 8;
 
-    stack_push(start, &disc8);
-    stack_push(start, &disc7);
-    stack_push(start, &disc6);
-    stack_push(start, &disc5);
-    stack_push(start, &disc4);
     stack_push(start, &disc3);
     stack_push(start, &disc2);
     stack_push(start, &disc1);
@@ -46,8 +34,8 @@ int hanoi() {
     initialize(&start, &temp, &goal);
 
     size_t size = stack_size(&start);
-    if (size != 8) {
-        printf("size of start should be %d but is %zu\n", 8, size);
+    if (size != 3) {
+        printf("size of start should be %d but is %zu\n", 3, size);
         return TEST_FAILURE;
     }
 
@@ -57,7 +45,7 @@ int hanoi() {
         return TEST_FAILURE;
     }
 
-    solve(&start, &goal, &temp, DISCS);
+    solve(&start, &goal, &temp, 3);
 
     size = stack_size(&start);
     if (size != 0) {
@@ -66,8 +54,8 @@ int hanoi() {
     }
 
     size = stack_size(&goal);
-    if (size != 8) {
-        printf("size of goal should be %d but is %zu\n", 8, size);
+    if (size != 3) {
+        printf("size of goal should be %d but is %zu\n", 3, size);
         return TEST_FAILURE;
     }
 
@@ -80,6 +68,18 @@ int hanoi() {
     stack_pop(&goal, (void*)&next_disc);
     if (*next_disc != 2) {
         printf("next disc of goal should be %d but is %d\n", 2, *next_disc);
+        return TEST_FAILURE;
+    }
+
+    stack_pop(&goal, (void*)&next_disc);
+    if (*next_disc != 3) {
+        printf("next disc of goal should be %d but is %d\n", 3, *next_disc);
+        return TEST_FAILURE;
+    }
+
+    int success = stack_pop(&goal, (void*)&next_disc);
+    if (success != -1) {
+        printf("popping from empty list should not be successful\n");
         return TEST_FAILURE;
     }
 
